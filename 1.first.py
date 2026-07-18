e5b5d4c39f768da3904d450fdac982bca3c626c8
Sahil002620Q <smax06388@gmail.com>
2026-07-18 15:38:00 +0530

    first programme using panda lib , covered panda series , loc and iloc

---
+++ 1.first.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = ['Create','Read','Update','Read']
+
+series = pd.Series(data,index = ['C','R','U','D'])
+print(series)
+
+#change value
+series.loc['C'] = 'CREATE'
+
+print(series.loc['C'])    # loc -> location (by real index)
+print(series.iloc[0])   # iloc -> integer location (always by 0 indexing)
+
+dict_data = {'key':'val'}
+
+series_x2 = pd.Series(dict_data)
+print(series_x2)
+print(series_x2.iloc[0])   #no indexing jut key value or iloc
